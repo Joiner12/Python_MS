985151f238d383c5ef0fb1118b36836cdac4a701
Risky Jr <2114768901@qq.com>
2021-01-08 20:49:41 +0800

    主页面修改为无边框模式

--- ToolInOne/Script/Wifi.py
+++ ToolInOne/Script/Wifi.py
@@ -2,10 +2,34 @@
 __author__ = "Peace4lv"
 __date__ = "2021年1月3日"
 
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+import sys
+from buttonstyle import*
 
-class Wifi():
+
+class Wifi(QWidget):
     def __init__(self):
+        super().__init__()
+        self.setupUi()
         print("netsh wlan show profile")
+
+    # ui setting
+    def setupUi(self):
+        # set button style
+        button = QPushButton("WIFI密码查看")
+        button.setStyleSheet(buttonStyle_1)
+        mainlayout = QVBoxLayout()
+        mainlayout.addWidget(button)
+        self.setLayout(mainlayout)
 
     def ShowName(self):
         print("我想对你说")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    demo = Wifi()
+    demo.show()
+    sys.exit(app.exec_())
---
+++ ToolInOne/Script/buttonstyle.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8-*-
+__author__ = "Peace4Lv"
+__date__ = "2021-01-05"
+
+# 按钮样式-1
+buttonStyle_1 = (
+    "QPushButton{border-radius:4px;font-size:16px;font-weight:bold;color:rgb(2, 9, 34);}"
+    "QPushButton{border:2px solid rgb(118,154,40);}"
+    "QPushButton:hover{background:rgb(118,154,40);}"
+)
+
+# 按钮样式-2 enable = False
+buttonStyle_2 = (
+    "QPushButton{border-radius:4px;font-size:16px;font-weight:bold;color:#ff5500;}"
+    "QPushButton{border:2px solid rgb(118,154,40);}"
+    "QPushButton:hover{background:rgb(118,154,40);}"
+)
+
+buttonStyle_3 = (
+    "QPushButton{border-radius:4px;font-size:16px;font-weight:bold;color:#ff5500;}"
+    "QPushButton{border:2px solid rgb(118,154,40);}"
+    "QPushButton:hover{background:rgb(118,154,40);}"
+)
+
+# select area style
+selectAreaStyle = (
+    "QListWidget{font-size:16px;font-weight:bold;color:rgb(67, 9, 34);}"
+    "QListWidget{border:4px;border-radius:4px;}"
+    "QListWidget{background-color:transparent;}"
+)
