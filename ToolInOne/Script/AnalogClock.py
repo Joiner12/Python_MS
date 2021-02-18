b50eaf3f96131eee40045e54f179f95f65c311b9
Risky Jr <2114768901@qq.com>
2021-02-18 23:05:32 +0800

    合并表盘时钟修改

--- ToolInOne/Script/AnalogClock.py
+++ ToolInOne/Script/AnalogClock.py
@@ -1,13 +1,18 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 
 __author__ = "Peace4Lv"
 __date__ = "2021-2-18"
 
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton
-from PyQt5.QtGui import QIcon, QPixmap, QPainter,QPainterPath
+from PyQt5.QtGui import QIcon, QPixmap, QPainter, QPainterPath
 from PyQt5.QtCore import Qt,  QTimer
 import pathlib
 import sys
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+
 
 def pathConfig():
     retpath = list()
@@ -48,37 +53,80 @@ class ClockDial(QWidget):
         imgFile = imgFile.joinpath("表盘-1.jpg")
         if imgFile.exists():
             imgFile = imgFile.__str__()
-    print("image file config finished",imgFile)
+    print("image file config finished", imgFile)
+
+    hourHand = QtGui.QPolygon([
+        QtCore.QPoint(4, 10),
+        QtCore.QPoint(-4, 10),
+        QtCore.QPoint(0, -50)
+    ])
+    minuteHand = QtGui.QPolygon([
+        QtCore.QPoint(4, 8),
+        QtCore.QPoint(-4, 8),
+        QtCore.QPoint(0, -70)
+    ])
+    hourColor = QtGui.QColor(33, 211, 230)
+    minuteColor = QtGui.QColor(10, 85, 93)
 
     def __init__(self):
         super().__init__()
-        mainTimer = QTimer()
-        mainTimer.timeout.connect(self.update)
-        # mainTimer.start(1000)  # 1000ms
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.update)
+        timer.start(1000)
+        self.setWindowTitle("Analog Clock")
+        self.resize(200, 200)
 
     def paintEvent(self, event):
-        qp = QPainter()
-        qp.begin(self)
-        # qp.setRenderHint(QPainter.Antialiasing) #抗锯齿
-        side = min(self.width(), self.height())
-        qp.translate(self.width()/2, self.height()/2)
-        self.target = QPixmap(self.size())
-        self.target.fill(Qt.transparent)
-        imgB = self.imgFile
-        self.radius = 100
-        p = QPixmap(imgB).scaled(200,200,Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation)
-        qp.setRenderHint(QPainter.Antialiasing, True)
-        qp.setRenderHint(QPainter.HighQualityAntialiasing, True)
-        qp.setRenderHint(QPainter.SmoothPixmapTransform, True)
-        path = QPainterPath()
-        path.addRoundedRect(
-            0, 0, self.width(), self.height(), self.radius, self.radius)
-        qp.setClipPath(path)
-        qp.drawPixmap(0, 0, p)
-        self.setPixmap(self.target)
-        qp.end()
+        side = min(self.width(), self.height())  # 最小边
+        time = QtCore.QTime.currentTime()  # 获取系统当前时间
 
-    # def drawImg(self,event,qp):
+        painter = QtGui.QPainter(self)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)  # 抗锯齿
+        painter.translate(self.width() / 2, self.height() / 2)  # 坐标偏置
+        painter.scale(side / 300.0, side / 300.0)  # 坐标系缩放
+
+        painter.setPen(QtGui.QColor(0, 0, 0))
+        painter.drawEllipse(-100, -100, 200, 200)  # 画圆。参数是外接矩形左上点和长宽
+
+        painter.setPen(QtCore.Qt.NoPen)
+        painter.setBrush(self.hourColor)
+
+        # 表盘
+        painter.save()
+        self.target = QtGui.QPixmap(self.size())
+        self.target.fill(Qt.transparent)
+        if False:
+            imgB = r"D:\Code\Python\ToolInOne\Doc\Pics\表盘-1.jpg"
+        else:
+            imgB = self.imgFile
+        self.radius = 10
+        p = QtGui.QPixmap(imgB).scaled(self.width()/2, self.height()/2,
+                                       Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing, True)
+        painter.setRenderHint(QtGui.QPainter.HighQualityAntialiasing, True)
+        painter.setRenderHint(QtGui.QPainter.SmoothPixmapTransform, True)
+        path = QtGui.QPainterPath()
+        path.addRoundedRect(
+            -self.width()/2, -self.height()/2, self.width(), self.height(), self.radius, self.radius)
+        painter.setClipPath(path)
+        painter.drawPixmap(-self.width()/2, -self.height()/2, p)
+        painter.restore()
+
+        painter.save()
+        painter.rotate(30.0 * ((time.hour() + time.minute() / 60.0)))
+        painter.drawConvexPolygon(self.hourHand)  # 画三角形
+        painter.restore()
+
+        painter.setPen(QtCore.Qt.NoPen)
+        painter.setBrush(self.minuteColor)
+        painter.save()
+
+        painter.rotate(6.0 * (time.minute() + time.second() / 60.0))
+        painter.drawConvexPolygon(self.minuteHand)
+        painter.restore()
+
+        painter.setPen(QtGui.QColor(0, 0, 0))
+        painter.drawEllipse(-5, -5, 10, 10)  # 画圆。参数是外接矩形左上点和长宽
 
 
 if __name__ == "__main__":
--- ToolInOne/Script/pyticker.py
+++ ToolInOne/Script/pyticker.py
@@ -6,13 +6,13 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 
 class AnalogClock(QWidget):
     hourHand = QtGui.QPolygon([
-        QtCore.QPoint(10, 8),
-        QtCore.QPoint(-10, 8),
-        QtCore.QPoint(0, -60)
+        QtCore.QPoint(4, 6),
+        QtCore.QPoint(-4, 6),
+        QtCore.QPoint(0, -50)
     ])
     minuteHand = QtGui.QPolygon([
-        QtCore.QPoint(8, 8),
-        QtCore.QPoint(-8, 8),
+        QtCore.QPoint(4, 4),
+        QtCore.QPoint(-4, 4),
         QtCore.QPoint(0, -70)
     ])
     hourColor = QtGui.QColor(255, 0, 0)
@@ -27,13 +27,13 @@ class AnalogClock(QWidget):
         self.resize(200, 200)
 
     def paintEvent(self, event):
-        side = min(self.width(), self.height()) # 最小边
+        side = min(self.width(), self.height())  # 最小边
         time = QtCore.QTime.currentTime()  # 获取系统当前时间
 
         painter = QtGui.QPainter(self)
         painter.setRenderHint(QtGui.QPainter.Antialiasing)  # 抗锯齿
-        painter.translate(self.width() / 2, self.height() / 2) # 坐标偏置
-        painter.scale(side / 300.0, side / 300.0) # 坐标系缩放
+        painter.translate(self.width() / 2, self.height() / 2)  # 坐标偏置
+        painter.scale(side / 300.0, side / 300.0)  # 坐标系缩放
 
         painter.setPen(QtGui.QColor(0, 0, 0))
         painter.drawEllipse(-100, -100, 200, 200)  # 画圆。参数是外接矩形左上点和长宽
@@ -47,7 +47,8 @@ class AnalogClock(QWidget):
         self.target.fill(Qt.transparent)
         imgB = r"D:\Code\Python\ToolInOne\Doc\Pics\表盘-1.jpg"
         self.radius = 10
-        p = QtGui.QPixmap(imgB).scaled(self.width(),self.height(),Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation)
+        p = QtGui.QPixmap(imgB).scaled(self.width(), self.height(
+        ), Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation)
         painter.setRenderHint(QtGui.QPainter.Antialiasing, True)
         painter.setRenderHint(QtGui.QPainter.HighQualityAntialiasing, True)
         painter.setRenderHint(QtGui.QPainter.SmoothPixmapTransform, True)
@@ -60,19 +61,20 @@ class AnalogClock(QWidget):
 
         painter.save()
         painter.rotate(30.0 * ((time.hour() + time.minute() / 60.0)))
-        painter.drawConvexPolygon(AnalogClock.hourHand) # 画三角形
+        painter.drawConvexPolygon(AnalogClock.hourHand)  # 画三角形
         painter.restore()
 
         painter.setPen(QtCore.Qt.NoPen)
         painter.setBrush(AnalogClock.minuteColor)
         painter.save()
-    
+
         painter.rotate(6.0 * (time.minute() + time.second() / 60.0))
         painter.drawConvexPolygon(AnalogClock.minuteHand)
         painter.restore()
 
         painter.setPen(QtGui.QColor(0, 0, 0))
         painter.drawEllipse(-5, -5, 10, 10)  # 画圆。参数是外接矩形左上点和长宽
+
 
 if __name__ == '__main__':
     import sys
