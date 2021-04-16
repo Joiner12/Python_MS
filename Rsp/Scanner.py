c926fca0eb747e1bad5a64fe5c9138d34edbfc07
Risky Jr <2114768901@qq.com>
2021-04-16 17:01:28 +0800

    scanner

---
+++ Rsp/Scanner.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# ConstMacHead = "40:D6:3C:00"
+# dev = "40:D6:3C:00:0A:49"
+# b = dev.find(ConstMacHead)
+# deb = 1
+
+# if __name__ == "__main__":
+#     while True:
+#         ipChar = input()
+#         if ipChar == "stop":
+#             break
